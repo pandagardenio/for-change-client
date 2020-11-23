8c912ebad17b5b3ecd85fdb7aa6d789ecae0d1e4
Luis Grande Garcia <luis.grande.garcia@gmail.com>
2020-11-23 11:15:20 +0100

    feat: 🎸 group online places by type

--- src/components/Places/OnlinePlaces.tsx
+++ src/components/Places/OnlinePlaces.tsx
@@ -1,7 +1,8 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, Typography } from '@material-ui/core';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-import { Place } from '../../sdk/models/Place';
+import { Place, PlaceType } from '../../sdk/models/Place';
 import { PlaceCard } from '../PlaceCard';
 
 export type OnlinePlacesProps = {
@@ -18,21 +19,53 @@ const useStyles = makeStyles((theme: Theme) =>
             padding: 0
         },
         element: {
+            margin: theme.spacing(1),
             width: theme.spacing(40)
         }
     }),
 );
 
+type GroupedPlacesByType = {
+    [PlaceType.ASSOCIATIONS]?: Place[];
+    [PlaceType.CLOTHING]?: Place[];
+    [PlaceType.EVENTS]?: Place[];
+    [PlaceType.GROCERIES]?: Place[];
+    [PlaceType.SHOPPING]?: Place[];
+}
+
 export const OnlinePlaces: React.FunctionComponent<OnlinePlacesProps> = (
     { places }: OnlinePlacesProps
 ): JSX.Element => {
     const classes = useStyles();
+    const { t } = useTranslation();
+
+    const getGroupedPlacesByType = (): GroupedPlacesByType => {
+        return places.reduce((groupedPlacesByType: GroupedPlacesByType, place: Place): Partial<GroupedPlacesByType> => {
+            let placesByPlaceType = groupedPlacesByType[place.type];
+
+            if (!placesByPlaceType) {
+                placesByPlaceType = [];
+            }
+
+            placesByPlaceType.push(place);
+
+            return {
+                ...groupedPlacesByType,
+                [place.type]: placesByPlaceType
+            }
+        }, {});
+    };
 
     return (
         <>
-        <ul className={classes.root}>
-            {places.map((place: Place) => <li className={classes.element}><PlaceCard place={place}/></li>)}
-        </ul>
+            {Object.keys(getGroupedPlacesByType()).map((placeType: string): JSX.Element => (
+                <>
+                    <Typography variant="h4">{t(`place.type.${placeType}`)}</Typography>
+                    <ul className={classes.root}>
+                        {places.map((place: Place) => <li className={classes.element}><PlaceCard place={place}/></li>)}
+                    </ul>
+                </>
+            ))}
         </>
     );
 };
